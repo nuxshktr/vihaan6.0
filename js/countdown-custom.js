fa40fc131c636ddbce09c709b080bed11089adf8
Himanshu Kumar <68270886+hi-ky@users.noreply.github.com>
2023-03-23 00:38:29 +0530

    Delete js directory

--- js/countdown-custom.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-        $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2023, 5, 20, 8)}); // year, month, date, hour
-        });
-});		
-
