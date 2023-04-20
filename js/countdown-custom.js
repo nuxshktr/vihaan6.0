89b79c0af4352b9f4ffff3f4380451c0e542dd4e
Himanshu Kumar <68270886+hi-ky@users.noreply.github.com>
2023-04-20 12:52:46 +0530

    Update countdown-custom.js
    /dates

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2023, 3, 26, 0)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2023, 3, 24, 0)}); // year, month, date, hour
         });
 });		
 
